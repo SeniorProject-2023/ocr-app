af7111b6226a2c92b71e7c26f9af0f9bf5aa879b
Youssef Victor <youssefvictor00@gmail.com>
2023-03-24 16:50:04 +0200

    More work

--- OCRApp/ViewModels/HomeViewModel.cs
+++ OCRApp/ViewModels/HomeViewModel.cs
@@ -1,21 +1,35 @@
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace OCRApp.ViewModels;
 
 public sealed class HomeViewModel : BindableBase
 {
-    public ObservableCollection<BitmapImage> ImagesToScan { get; } = new();
+    public ObservableCollection<ByteArrayWrapper> ImagesToScan { get; } = new();
 
     private Page _activePage;
+    private int _selectedIndex;
+
     public Page ActivePage
     {
         get => _activePage;
-        
+
         [MemberNotNull(nameof(_activePage))]
         set => SetProperty(ref _activePage!, value);
+    }
+
+    public int SelectedIndex
+    {
+        get => _selectedIndex;
+        set
+        {
+            SetProperty(ref _selectedIndex, value);
+            foreach (var image in ImagesToScan)
+            {
+                image.NotifyVisibilityChanged();
+            }
+        }
     }
 
     public HomeViewModel()
