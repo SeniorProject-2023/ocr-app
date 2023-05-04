69f375332f01cf899c2edea10606728a8d2e92d1
Youssef Victor <youssefvictor00@gmail.com>
2023-05-04 22:35:05 +0300

    Adjust

--- src/Backend/arabic_ocr_backend/urls.py
+++ src/Backend/arabic_ocr_backend/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include('home.urls')),
     path("admin/", admin.site.urls),
     path("api/", include('ocr_api.urls')),
     path("users/", include('users.urls'))
