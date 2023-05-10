bb59e8ecea96ff51219b5121ad9afc34e2c876e8
MostafaTwfiq <mostafa.mohammad.twfiq@gmail.com>
2023-05-10 22:10:32 +0300

    Integrated model inference into backend

--- src/Backend/models/test.py
+++
@@ -1,5 +0,0 @@
-from pylabel import importer
-
-dataset = importer.ImportCoco(path="/home/astroc/Projects/Python/Graduation/SegmentationGeneration/book.json", 
-                              path_to_images="/home/astroc/Projects/Python/Graduation/SegmentationGeneration/book")
-dataset.export.ExportToYoloV5(output_path="./yolo/labels", copy_images=True)
