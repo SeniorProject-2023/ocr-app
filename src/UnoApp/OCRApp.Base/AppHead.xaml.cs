35332416810a7bf31e91fcebff764f5138efd968
Youssef Victor <youssefvictor00@gmail.com>
2023-05-15 23:05:04 +0300

    Move to file-scoped again

--- src/UnoApp/OCRApp.Base/AppHead.xaml.cs
+++ src/UnoApp/OCRApp.Base/AppHead.xaml.cs
@@ -1,14 +1,13 @@
-namespace OCRApp
+namespace OCRApp;
+
+public sealed partial class AppHead : App
 {
-    public sealed partial class AppHead : App
+    /// <summary>
+    /// Initializes the singleton application object. This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
+    public AppHead()
     {
-        /// <summary>
-        /// Initializes the singleton application object. This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public AppHead()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
     }
 }
