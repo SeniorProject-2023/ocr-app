b70c2c3fad5e32fd5d378df2f22852bb080f0937
Youssef1313 <Youssef1313@users.noreply.github.com>
2023-06-06 13:28:16 +0000

    deploy: 366a4e10484c5a10b31a1102a77da6a1ec53206b

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-import { config } from "./package_8da710e77dff5e24f0ef42d597fccf8a29cc13f3//uno-config.js";
+import { config } from "./package_d833240ff904d63c6e6bcd801beab2df46f9ea00//uno-config.js";
 
 console.debug("[ServiceWorker] Initializing");
 
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        caches.open('package_8da710e77dff5e24f0ef42d597fccf8a29cc13f3').then(async function (cache) {
+        caches.open('package_d833240ff904d63c6e6bcd801beab2df46f9ea00').then(async function (cache) {
             console.debug('[ServiceWorker] Caching app binaries and content');
 
             // Add files one by one to avoid failed downloads to prevent the
