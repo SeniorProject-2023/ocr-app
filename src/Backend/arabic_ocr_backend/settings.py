ff1ecc5d245db1a37a623562cad90094d56386e0
Mostafa Tawfiq <37211598+MostafaTwfiq@users.noreply.github.com>
2023-06-12 12:50:47 +0000

    Added custom password validations

--- src/Backend/arabic_ocr_backend/settings.py
+++ src/Backend/arabic_ocr_backend/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "ocr_api",
     "corsheaders",
-    "users"
+    "users", 
+    "users.validators"
 ]
 
 MIDDLEWARE = [
@@ -115,12 +116,39 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "OPTIONS": {
+            "min_length": 8,
+        }
     },
     {
         "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+    {
+        "NAME": "users.validators.pass_validators.UppercaseValidator",
+        "OPTIONS": {
+            "min_upper": 1,
+        }
+    },
+    {
+        "NAME": 'users.validators.pass_validators.LowercaseValidator',
+        "OPTIONS": {
+            "min_lower": 3,
+        }
+    },
+    {
+        "NAME": "users.validators.pass_validators.NumericValidator",
+        "OPTIONS": {
+            "min_numeric": 3,
+        }
+    },
+    {
+        "NAME": "users.validators.pass_validators.SpecialCharValidator",
+        "OPTIONS": {
+            "min_special": 1,
+        }
     },
 ]
 
