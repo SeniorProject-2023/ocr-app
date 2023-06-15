6222fc7c9e2c356614c6d8bf0a3fd4515224aa10
Mostafa Tawfiq <37211598+MostafaTwfiq@users.noreply.github.com>
2023-06-15 15:54:36 +0000

    Removed cached

--- src/Backend/delete_deployments.bat
+++
@@ -1,3 +0,0 @@
-kubectl delete deployment ocr-backend
-kubectl delete deployment model-server
-kubectl delete services --all
--- src/Backend/deploy.bat
+++
@@ -1,8 +0,0 @@
-kubectl apply -f env-secret.yaml
-kubectl apply -f env-configmap.yaml
-kubectl apply -f ./OCR_Backend/deployment.yaml
-kubectl apply -f ./Model_Server/deployment.yaml
-REM kubectl create namespace backend
-kubectl apply -f ./OCR_Backend/service.yaml
-kubectl apply -f ./Model_Server/service.yaml
-REM kubectl expose deployment ocr-backend --type=LoadBalancer --name=public-ocr-backend --port=8000
